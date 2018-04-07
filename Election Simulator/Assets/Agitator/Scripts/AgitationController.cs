b314c653cb9a4e24f85b4c5318696c12264a845c
User3 <User3@k1607N0080392.kontur>
2018-04-07 17:13:43 +0500

    v.0.0.0.0.1

---
+++ Election Simulator/Assets/Agitator/Scripts/AgitationController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgitationController : MonoBehaviour {
+
+	PlayerController playerController;
+
+	void Start () {
+		playerController = transform.parent.GetComponent<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		if (!playerController.isAgitating) return;
+		if (!other.gameObject.tag.Equals("Human")) return;
+		var hc = other.gameObject.GetComponent<HumanController>();
+		if (!hc.isAgitated) hc.SetAgitation(1);
+	}
+}
---
+++ Election Simulator/Assets/Agitator/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject target;
+	public GameObject field;
+	
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		FollowTo(target.transform.position);
+	}
+
+	void FollowTo(Vector2 target) {
+		transform.position = new Vector3(target.x, target.y, transform.position.z);
+	}
+}
