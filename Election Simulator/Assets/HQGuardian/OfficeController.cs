dee231689dfd0a1efd0d976d36baff84683a9b52
Evgeniy <shook@live.ru>
2018-04-07 22:46:37 +0500

    Added Navalny office, controllers

---
+++ Election Simulator/Assets/HQGuardian/OfficeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OfficeController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerExit2D (Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			Debug.Log ("Tried to exit");
+			other.gameObject.GetComponent<ChiefController>().NextTarget ();
+		}
+	}
+}
