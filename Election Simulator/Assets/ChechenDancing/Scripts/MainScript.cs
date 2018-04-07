a75c92e09e8db42b5850a2458d89e8431fc647ac
Sergey Kuzminov <derarvend@gmail.com>
2018-04-07 21:53:08 +0500

    add arrows, move sprites to resources

--- Election Simulator/Assets/ChechenDancing/Scripts/MainScript.cs
+++ Election Simulator/Assets/ChechenDancing/Scripts/MainScript.cs
@@ -3,33 +3,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainScript : MonoBehaviour {
-
-	private const int DefaultFallSpeed = 2;
-	private int arrowSpeed;
-	private Queue<GameObject> arrows;
-	private float secondDelay = 1;
-	public GameObject arrow;
-	void Start () {
-		StartCoroutine(arrowCreator());
-		//var i = Instantiate(arrow, new Vector2(12.5f, 5.5f), Quaternion.identity);
-		//var r = i.GetComponent<Rigidbody2D>();
-		//r.velocity = new Vector2(0, -2);
-	}
-
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-	IEnumerator arrowCreator()
+public class MainScript : MonoBehaviour
+{
+	private const float lengthInArrowTicks = 60;
+	public GameObject arrowPrefab;
+	private System.Random random;
+	void Start ()
 	{
-		while (true)
+		random = new System.Random ();
+		StartCoroutine (arrowCreator ());
+	}
+
+	void Update ()
+	{
+
+	}
+
+	IEnumerator arrowCreator ()
+	{
+		var ticksElapsed = 0;
+		float velocityModifier = 1;
+		while (ticksElapsed < lengthInArrowTicks)
 		{
-			var instance = Instantiate(arrow, new Vector2(12.5f, 5.5f), Quaternion.identity);
-			var r = instance.GetComponent<Rigidbody2D>();
-			r.velocity = new Vector2(0, -3);
-			yield return new WaitForSecondsRealtime(secondDelay);
+			var instance = Instantiate (arrowPrefab);
+			var direction = GetRandomDirection ();
+			instance.GetComponent<ArrowScript> ().Direction = GetRandomDirection ();
+
+			var r = instance.GetComponent<Rigidbody2D> ();
+			r.velocity = new Vector2 (-2 * velocityModifier, 0);
+
+			var delay = GetRandomDelay();
+			ticksElapsed += delay;
+
+			if (ticksElapsed % 10 == 0)
+				velocityModifier = 1 + ticksElapsed / lengthInArrowTicks;
+
+
+			yield return new WaitForSeconds ((float) delay / 2);
 		}
 	}
-} 
+
+	int GetRandomDelay ()
+	{
+		return random.Next (1, 4);
+	}
+
+	Direction GetRandomDirection ()
+	{
+		var values = Enum.GetValues (typeof (Direction));
+		var result = (Direction) values.GetValue (random.Next (values.Length));
+		return result;
+	}
+
+}
--- Election Simulator/Assets/ChechenDancing/Scripts/ScoreCircleScript.cs
+++ Election Simulator/Assets/ChechenDancing/Scripts/ScoreCircleScript.cs
@@ -1,21 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class ScoreCircleScript : MonoBehaviour {
+public class ScoreCircleScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-	void OnTriggerEnter2D(Collision2D col)
+	HashSet<GameObject> currentCollidedObjects;
+	void Start ()
 	{
-		Destroy(col.otherCollider.gameObject);
+		currentCollidedObjects = new HashSet<GameObject> ();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	void FixedUpdate ()
+	{
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		currentCollidedObjects.Add (other.gameObject);
+	}
+	void OnTriggerExit2D (Collider2D other)
+	{
+		currentCollidedObjects.Remove (other.gameObject);
+		Destroy (other.gameObject);
 	}
 }
