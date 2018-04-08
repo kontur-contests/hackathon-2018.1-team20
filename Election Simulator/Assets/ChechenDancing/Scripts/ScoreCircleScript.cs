416ab314c5154d8719d2425d93f4f4f5e093024e
Sergey Kuzminov <derarvend@gmail.com>
2018-04-08 06:09:58 +0500

    add square blinking

--- Election Simulator/Assets/ChechenDancing/Scripts/ScoreCircleScript.cs
+++ Election Simulator/Assets/ChechenDancing/Scripts/ScoreCircleScript.cs
@@ -6,12 +6,8 @@ using UnityEngine;
 public class ScoreCircleScript : MonoBehaviour
 {
 	public GameObject Logic;
-	private Dictionary<KeyCode, Direction> keyToDirection = new Dictionary<KeyCode, Direction>()
-	{
-		{KeyCode.LeftArrow, Direction.Left},
-		{KeyCode.RightArrow, Direction.Right},
-		{KeyCode.UpArrow, Direction.Up},
-		{KeyCode.DownArrow, Direction.Down},
+	private Dictionary<KeyCode, Direction> keyToDirection = new Dictionary<KeyCode, Direction> ()
+	{ { KeyCode.LeftArrow, Direction.Left }, { KeyCode.RightArrow, Direction.Right }, { KeyCode.UpArrow, Direction.Up }, { KeyCode.DownArrow, Direction.Down },
 	};
 	HashSet<GameObject> currentCollidedObjects;
 	void Start ()
@@ -28,25 +24,33 @@ public class ScoreCircleScript : MonoBehaviour
 	void FixedUpdate ()
 	{
 		bool wasInput = false;
-		foreach(var kv in keyToDirection){
-			if (Input.GetKeyDown(kv.Key)){
+		foreach (var kv in keyToDirection)
+		{
+			if (Input.GetKeyDown (kv.Key))
+			{
 				wasInput = true;
-				var collided = currentCollidedObjects.FirstOrDefault(
-				o => o.GetComponent<ArrowScript>().Direction == kv.Value);
+				var collided = currentCollidedObjects.FirstOrDefault (
+					o => o.GetComponent<ArrowScript> ().Direction == kv.Value);
 
-				if (collided != null) {
-					Logic.GetComponent<MainScript>().IncrementScore(); 
-					currentCollidedObjects.Remove(collided);
+				if (collided != null)
+				{
+					Logic.GetComponent<MainScript> ().IncrementScore ();
+					this.Blink (Color.green, 0.25f);
+					currentCollidedObjects.Remove (collided);
 					return;
 				}
 			}
 		}
-		if (wasInput) Logic.GetComponent<MainScript>().DecrementScore();
-		
+		if (wasInput)
+		{
+			Logic.GetComponent<MainScript> ().DecrementScore ();
+			this.Blink (Color.red, 0.25f);
+		}
 	}
 
-	void HandleKey() {
-		
+	void HandleKey ()
+	{
+
 	}
 
 	void OnTriggerEnter2D (Collider2D other)
@@ -57,5 +61,18 @@ public class ScoreCircleScript : MonoBehaviour
 	{
 		currentCollidedObjects.Remove (other.gameObject);
 		Destroy (other.gameObject);
+	}
+
+	public void Blink (Color color, float delayInSeconds)
+	{
+		StartCoroutine (BlinkCoroutine (color, delayInSeconds));
+	}
+	private IEnumerator BlinkCoroutine (Color color, float delayInSeconds)
+	{
+		var spriteRenderer = this.gameObject.GetComponent<SpriteRenderer> ();
+		if (color == Color.red) spriteRenderer.sprite = Resources.Load<Sprite> ("CechenDancer/arrow-square-red");
+		else if (color == Color.green) spriteRenderer.sprite = Resources.Load<Sprite> ("CechenDancer/arrow-square-green");
+		yield return new WaitForSeconds (delayInSeconds);
+		spriteRenderer.sprite = Resources.Load<Sprite> ("CechenDancer/arrow-square");
 	}
 }
